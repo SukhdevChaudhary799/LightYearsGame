df2dcd0062989671d7936e2f5ffd1b1fd7543efd
sukhdevChaudhary <sukhdevchaudhary799@gmail.com>
2026-01-30 23:35:43 +0530

    chore(Core.h): added more typenames

--- LightYears/LightYearsEngine/include/framework/Core.h
+++ LightYears/LightYearsEngine/include/framework/Core.h
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <memory>
+#include <map>
+#include <unordered_map>
 
 namespace ly{
+
+template<typename T>
+using unique = std::unique_ptr<T>;
+
+template<typename T>
+using shared = std::shared_ptr<T>;
+
+template<typename T>
+using weak = std::weak_ptr<T>;
+
+template<typename T>
+using List = std::vector<T>;
+
+template<typename keyType, typename valueType, typename hash = std::hash<keyType>>
+using Map = std::map<keyType, valueType, hash>;
+
+template<typename keyType, typename valueType, typename hasher = std::hash<keyType>>
+using Dictionary = std::unordered_map<keyType, valueType, hasher>;
+
     
 #define LOG(M, ...) printf(M "\n", ##__VA_ARGS__)
 
